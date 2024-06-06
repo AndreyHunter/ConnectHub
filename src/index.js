538d9af2787138f61c034343e9437872c73c3098
Andrey <andreybespaliy69@gmail.com>
2024-06-06 23:59:22 +0300

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import store from './store/index';
 import { Provider, connect } from 'react-redux';
